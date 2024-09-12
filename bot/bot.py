da15a98fb0e7bbb2b500b8b6bcdb642eace0e12f
Silva Silver <reichstayl20@gmail.com>
2024-09-12 22:46:13 +0300

    2 src works but no descriptions

--- bot/bot.py
+++ bot/bot.py
@@ -16,19 +16,18 @@ with open('config.json', 'r') as file:
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-# URL-адрес поиска для второго источника
+# URL-адреса поиска
 SEARCH_URL_LORDSERIAL = 'https://lordserial.run/index.php?do=search'
+SEARCH_URL_GOODFILMS = 'https://zhqpg.goodfilms.fun/index.php?do=search'
 
 # Глобальные переменные для хранения результатов поиска и избранного
 search_results_cache = {}
-
 
 # Подключение к базе данных SQLite
 def get_db_connection():
     conn = sqlite3.connect('favorites.db')
     conn.row_factory = sqlite3.Row
     return conn
-
 
 # Создание таблиц, если они не существуют
 def create_tables():
@@ -44,76 +43,25 @@ def create_tables():
     ''')
     conn.close()
 
-
 create_tables()
 
-
 # Функция для получения HTML-кода страницы
-def get_page(url: str, params: dict = None) -> str:
-    try:
-        response = requests.get(url, params=params)
-        response.raise_for_status()  # Проверка успешности запроса
-        return response.text
-    except requests.RequestException as e:
-        print(f"Ошибка при получении страницы: {e}")
-        return ""
+def get_page(url, params=None):
+    response = requests.get(url, params=params)
+    response.raise_for_status()  # Проверяем на ошибки
+    # Выводим первые 1000 символов HTML-кода для отладки
+    logger.debug(f'HTML код страницы: {response.text[:1000]}')
+    return response.text
 
 
-# Функция для поиска фильмов на Goodfilms
-def search_goodfilms(query):
-    url = 'https://zhqpg.goodfilms.fun/index.php?do=search'
-    payload = {
-        'do': 'search',
-        'subaction': 'search',
-        'search_start': '0',
-        'full_search': '0',
-        'result_from': '1',
-        'story': query
-    }
+# Функция для получения результатов поиска с сайта lordserial
+def get_search_results_lordserial(search_term):
+    params_lordserial = {'do': 'search', 'subaction': 'search', 'story': search_term}
+    search_content_lordserial = get_page(SEARCH_URL_LORDSERIAL, params=params_lordserial)
+    return parse_search_results_lordserial(search_content_lordserial)
 
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)',
-        'Content-Type': 'application/x-www-form-urlencoded',
-    }
-
-    response = requests.post(url, data=payload, headers=headers)
-
-    soup = BeautifulSoup(response.content, 'html.parser')
-
-    results = []
-    for item in soup.find_all('div', class_='poster has-overlay grid-item d-flex fd-column'):
-        title = item.find('span', class_='poster__title').text.strip()
-        link = item.find('a', class_='poster__link')['href']
-        genres = item.find('div', class_='poster__subtitle').text.strip()
-        ratings = item.find('div', class_='poster__ratings').text.strip()
-        image = item.find('img')['src']
-
-        results.append({
-            'title': title,
-            'link': 'https://zhqpg.goodfilms.fun' + link,
-            'genres': genres,
-            'ratings': ratings,
-            'image': 'https://zhqpg.goodfilms.fun' + image
-        })
-
-    return results
-
-
-# Функция для получения плеера фильма
-def get_movie_player(url):
-    response = requests.get(url)
-    soup = BeautifulSoup(response.content, 'html.parser')
-
-    video_tag = soup.find('video')
-    if video_tag:
-        video_src = video_tag['src']
-        track = video_tag.find('track')['src'] if video_tag.find('track') else None
-        return {'video_src': video_src, 'track': track}
-    return None
-
-
-# Функция для парсинга результатов поиска на LordSerial
-def parse_search_results(content):
+# Функция для парсинга результатов поиска с сайта lordserial
+def parse_search_results_lordserial(content):
     soup = BeautifulSoup(content, 'html.parser')
     results = []
     for item in soup.find_all('div', class_='th-item'):
@@ -122,15 +70,45 @@ def parse_search_results(content):
         results.append((f"{title} (Источник 1)", link))
     return results
 
+# Функция для получения результатов поиска с сайта goodfilms
+def get_search_results_goodfilms(search_term):
+    params_goodfilms = {
+        'do': 'search',
+        'subaction': 'search',
+        'story': search_term,
+        'result_from': 1
+    }
+    search_content_goodfilms = requests.post(SEARCH_URL_GOODFILMS, data=params_goodfilms).text
+    return parse_search_results_goodfilms(search_content_goodfilms)
+
+# Функция для парсинга результатов поиска с сайта goodfilms
+def parse_search_results_goodfilms(content):
+    soup = BeautifulSoup(content, 'html.parser')
+    results = []
+    for item in soup.find_all('div', class_='poster'):
+        title = item.find('span', class_='poster__title').get_text(strip=True)
+        link = item.find('a', class_='poster__link')['href']
+        results.append((f"{title} (Источник 2)", link))
+    return results
+
+# Функция для получения результатов поиска из двух источников
+def get_search_results(search_term):
+    results_lordserial = get_search_results_lordserial(search_term)
+    results_goodfilms = get_search_results_goodfilms(search_term)
+    return results_lordserial + results_goodfilms
 
 # Функция для извлечения информации о фильме
 def extract_movie_info(movie_page_content):
     soup = BeautifulSoup(movie_page_content, 'html.parser')
-    title = soup.find('h1').get_text(strip=True)
-    description = soup.find('div', class_='fdesc').get_text(strip=True)
+    title = soup.find('h1')
+    description_div = soup.find('div', class_='fdesc')
+
+    title_text = title.get_text(strip=True) if title else 'Неизвестно'
+    description_text = description_div.get_text(strip=True) if description_div else 'Описание отсутствует'
+
     return {
-        'title': title,
-        'description': description,
+        'title': title_text,
+        'description': description_text,
     }
 
 
@@ -139,28 +117,22 @@ def extract_player_link(movie_page_content):
     soup = BeautifulSoup(movie_page_content, 'html.parser')
     iframe = soup.find('iframe')
     if iframe:
-        return iframe['src']
+        src = iframe['src']
+        if src.startswith('//'):
+            src = 'https:' + src
+        return src
+
     player_div = soup.find('div', class_='player-container')
     if player_div:
         player_link = player_div.find('a')
         if player_link and 'href' in player_link.attrs:
-            return player_link['href']
+            href = player_link['href']
+            if href.startswith('//'):
+                href = 'https:' + href
+            return href
+
     return None
 
-
-# Функция для получения результатов поиска
-def get_search_results(search_term):
-    # Получаем результаты из первого источника
-    results_goodfilms = search_goodfilms(search_term)
-
-    # Получаем результаты из второго источника
-    params_lordserial = {'do': 'search', 'subaction': 'search', 'story': search_term}
-    search_content_lordserial = get_page(SEARCH_URL_LORDSERIAL, params=params_lordserial)
-    results_lordserial = parse_search_results(search_content_lordserial)
-
-    # Объединяем результаты
-    combined_results = [(result['title'], result['link']) for result in results_goodfilms] + results_lordserial
-    return combined_results
 
 
 # Функция для создания клавиатуры с кнопками
@@ -180,7 +152,6 @@ def build_keyboard(results, current_page, total_pages):
 
     return InlineKeyboardMarkup(keyboard)
 
-
 # Функция для создания клавиатуры с кнопками на странице избранного
 def build_favorites_keyboard():
     keyboard = [
@@ -188,10 +159,8 @@ def build_favorites_keyboard():
     ]
     return InlineKeyboardMarkup(keyboard)
 
-
 def get_unique_id(url):
     return hashlib.md5(url.encode()).hexdigest()[:10]
-
 
 # Функция для создания клавиатуры на странице фильма
 def build_movie_keyboard(movie_url, player_url, is_favorite=False):
@@ -199,6 +168,7 @@ def build_movie_keyboard(movie_url, player_url, is_favorite=False):
     keyboard = []
 
     if player_url:
+        logger.debug(f'Player URL: {player_url}')
         keyboard.append([InlineKeyboardButton("▶️ СМОТРЕТЬ", url=player_url)])
     else:
         keyboard.append([InlineKeyboardButton("❓ Не найден плеер", callback_data='none')])
@@ -219,9 +189,7 @@ def get_user_favorites(chat_id):
     cursor = conn.execute('SELECT title, player_url FROM user_favorites WHERE chat_id = ?', (chat_id,))
     favorites = cursor.fetchall()
     conn.close()
-    return {'favorites': [row['title'] for row in favorites],
-            'links': {row['title']: row['player_url'] for row in favorites}}  # Используем player_url
-
+    return {'favorites': [row['title'] for row in favorites], 'links': {row['title']: row['player_url'] for row in favorites}}  # Используем player_url
 
 # Функция для обработки команды /start
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
@@ -249,48 +217,39 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
         parse_mode='HTML'
     )
 
-
 # Функция для обработки нажатия кнопки
+# Обновленный код для обработки нажатий на кнопку фильма
 async def button(update: Update, context: ContextTypes.DEFAULT_TYPE):
     query = update.callback_query
+    await query.answer()
     data = query.data
-    chat_id = query.message.chat_id
+    logger.info(f'Нажата кнопка с данными: {data}')
 
     if data == 'search':
-        await query.edit_message_text(
-            text="🔍 Введите название фильма или сериала, который вы хотите найти:"
-        )
-        return
+        await query.edit_message_text(text="Введите название фильма или сериала для поиска:")
 
-    if data == 'favorites':
-        favorites = get_user_favorites(chat_id)
-        if not favorites['favorites']:
-            await query.edit_message_text(
-                text="⚠️ В избранном пока ничего нет. Используйте поиск для добавления фильмов."
-            )
+    elif data == 'favorites':
+        chat_id = update.callback_query.message.chat_id
+        user_favorites = get_user_favorites(chat_id)
+
+        if not user_favorites['favorites']:
+            await query.edit_message_text('Избранные фильмы пусты.', reply_markup=build_favorites_keyboard())
         else:
-            favorite_titles = '\n'.join(favorites['favorites'])
-            await query.edit_message_text(
-                text=f"📚 Ваше избранное:\n{favorite_titles}",
-                reply_markup=build_favorites_keyboard()
-            )
-        return
+            favorites_message = '\n'.join([f"{idx + 1}. <a href='{user_favorites['links'][title]}'>{title}</a>"
+                                           for idx, title in enumerate(user_favorites['favorites'])])
+            await query.edit_message_text(f'Ваши избранные фильмы:\n{favorites_message}', parse_mode='HTML',
+                                          reply_markup=build_favorites_keyboard())
 
-    if data.startswith('movie_'):
+    elif data.startswith('movie_'):
         index = int(data.split('_')[1])
         results = search_results_cache.get('results', [])
         if 0 <= index < len(results):
             title, movie_url = results[index]
-            if 'goodfilms' in movie_url:
-                movie_page_content = get_page(movie_url)
-                movie_info = extract_movie_info(movie_page_content)
-                player_url = get_movie_player(movie_url).get('video_src')  # Используем новый метод
+            movie_page_content = get_page(movie_url)
+            movie_info = extract_movie_info(movie_page_content)
+            player_url = extract_player_link(movie_page_content)
 
-            else:
-                movie_page_content = get_page(movie_url)
-                movie_info = extract_movie_info(movie_page_content)
-                player_url = extract_player_link(movie_page_content)
-
+            chat_id = update.callback_query.message.chat_id
             conn = get_db_connection()
             cursor = conn.execute('SELECT 1 FROM user_favorites WHERE chat_id = ? AND url = ?', (chat_id, movie_url))
             is_favorite = cursor.fetchone() is not None
@@ -308,64 +267,68 @@ async def button(update: Update, context: ContextTypes.DEFAULT_TYPE):
                 parse_mode='HTML',
                 reply_markup=build_movie_keyboard(movie_url, player_url, is_favorite)
             )
-        return
 
-    if data.startswith('favorite_'):
+    elif data.startswith('favorite_'):
         unique_id = data.split('_')[1]
-        movie_url = next((url for url, uid in search_results_cache['results'] if get_unique_id(url) == unique_id), None)
+        movie_url = search_results_cache.get('url_map', {}).get(unique_id, '')
+        chat_id = update.callback_query.message.chat_id
 
         if movie_url:
-            conn = get_db_connection()
-            conn.execute('INSERT OR IGNORE INTO user_favorites (chat_id, title, url, player_url) VALUES (?, ?, ?, ?)',
-                         (chat_id, movie_url, movie_url, ''))
-            conn.commit()
-            conn.close()
+            movie_page_content = get_page(movie_url)
+            player_url = extract_player_link(movie_page_content)
 
-            await query.edit_message_text(
-                text="✅ Фильм добавлен в избранное!",
-                reply_markup=build_favorites_keyboard()
-            )
-        return
+            if player_url:
+                movie_info = extract_movie_info(movie_page_content)
 
-    if data.startswith('prev_') or data.startswith('next_'):
-        current_page = int(data.split('_')[1])
-        search_term = search_results_cache.get('search_term', '')
-        results = get_search_results(search_term)
-        search_results_cache['results'] = results
-        total_pages = (len(results) + 4) // 5
-        reply_markup = build_keyboard(results, current_page, total_pages)
-        await query.edit_message_text(
-            text=f"🔍 Результаты поиска по запросу '{search_term}':",
-            reply_markup=reply_markup
-        )
-        return
+                conn = get_db_connection()
+                conn.execute('''INSERT OR IGNORE INTO user_favorites (chat_id, title, url, player_url)
+                                VALUES (?, ?, ?, ?)''', (chat_id, movie_info['title'], movie_url, player_url))
+                conn.commit()
+                conn.close()
 
-    if data == 'home':
+                await query.answer("Фильм добавлен в избранное.", show_alert=True)
+                await query.edit_message_reply_markup(reply_markup=build_movie_keyboard(movie_url, player_url, True))
+
+        else:
+            await query.answer("Произошла ошибка. Попробуйте снова.", show_alert=True)
+
+    elif data == 'home':
         await start(update, context)
-        return
 
+    elif data.startswith('next_') or data.startswith('prev_'):
+        current_page = int(data.split('_')[1])
+        results = search_results_cache.get('results', [])
+        total_pages = search_results_cache.get('total_pages', 1)
+        await query.edit_message_reply_markup(reply_markup=build_keyboard(results, current_page, total_pages))
 
-# Функция для обработки текстовых сообщений (поиск)
-async def text_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
+# Функция для обработки текста от пользователя (поиск)
+async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
     search_term = update.message.text
-    if search_term:
-        search_results = get_search_results(search_term)
-        search_results_cache['results'] = search_results
-        search_results_cache['search_term'] = search_term
-        total_pages = (len(search_results) + 4) // 5
-        reply_markup = build_keyboard(search_results, 1, total_pages)
-        await update.message.reply_text(
-            text=f"🔍 Результаты поиска по запросу '{search_term}':",
-            reply_markup=reply_markup
-        )
+    chat_id = update.message.chat_id
 
+    results = get_search_results(search_term)
+    search_results_cache['results'] = results
+    search_results_cache['url_map'] = {get_unique_id(url): url for _, url in results}
 
-# Запуск бота
+    total_pages = (len(results) + 4) // 5
+    search_results_cache['total_pages'] = total_pages
+
+    if not results:
+        await context.bot.send_message(chat_id=chat_id, text="Результатов не найдено.")
+    else:
+        await context.bot.send_message(chat_id=chat_id, text="Результаты поиска:",
+                                       reply_markup=build_keyboard(results, current_page=1, total_pages=total_pages))
+
+# Функция для обработки ошибок
+async def error(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    logger.error('Произошла ошибка при обработке обновления:', exc_info=context.error)
+
 if __name__ == '__main__':
     application = Application.builder().token(TOKEN).build()
 
-    application.add_handler(CommandHandler("start", start))
-    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, text_handler))
+    application.add_handler(CommandHandler('start', start))
     application.add_handler(CallbackQueryHandler(button))
+    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
+    application.add_error_handler(error)
 
     application.run_polling()
